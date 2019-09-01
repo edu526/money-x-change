5452424b5523a15af32652af240c5272b776d935
Eduardo De la Cruz <edudelacruzrojas@gmail.com>
2019-09-01 12:30:56 -0500

    fss

---
+++ src/app/reducers/index.ts
@@ -0,0 +1,15 @@
+import {
+  ActionReducerMap,
+  MetaReducer
+} from '@ngrx/store';
+import { environment } from '../../environments/environment';
+
+export interface IState {
+
+}
+
+export const reducers: ActionReducerMap<IState> = {
+
+};
+
+export const metaReducers: Array<MetaReducer<IState>> = !environment.production ? [] : [];
